9784e11966a87fe876c135deaa97b1bee4277afe
Guillermo Jimenez Iriarte <122047330+GuilleJI@users.noreply.github.com>
2023-12-21 03:59:28 -0500

    added an empty array as fallback. followed by converting that JSON string into a JavaScript array..

--- main.js
+++ main.js
@@ -7,9 +7,10 @@ const ADD_BUTTON = document.getElementById("add");
 //Step 2: fetch the existing items from local storage.
 function getItems() {
     //this function will look at local storage and get the items out if there are already
-    const value = localStorage.getItem("todo-test");  //<----const value serves to get a specific value from local storage using "todo-test" as a pass, which refers to the key of the local storage key value pair (you can name it whatever you like really...)
+    const value = localStorage.getItem("todo-test") || "[]";  //<----const value serves to get a specific value from local storage using "todo-test" as a pass, which refers to the key of the local storage key value pair (you can name it whatever you like really...)
+                                                             //<---However, because we have no content from local storage after passing "todo-list" we can use solve this by adding an empty array by using  "||" followed by "[]". Great fallback to default in case it is first time user is opening up the page...
 
-    console.log(value); 
+    return JSON.parse(value); //<-----we need to convert that JSON string into an actual JavaScript array
 }
 
 //Once done, we call getItems functions 
