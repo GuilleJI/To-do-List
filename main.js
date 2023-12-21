29230e42890d8a33e358f81cc93b8768a58c524c
Guillermo Jimenez Iriarte <122047330+GuilleJI@users.noreply.github.com>
2023-12-21 03:30:33 -0500

    JAVASCRIPT: three elements from HTML are now referrenced by JavaScript using document.getElementById.()

---
+++ main.js
@@ -0,0 +1,5 @@
+// Step 1: 
+const ITEMS_CONTAINER = document.getElementById("items"); 
+const ITEM_TEMPLATE = document.getElementById("itemTemplate"); 
+const ADD_BUTTON = document.getElementById("add"); 
+// All three (items, itemTemplate, and "add" button are referenced in out JavaScript file)
