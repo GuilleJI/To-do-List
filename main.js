17d359795c066440c02744e5350500352c5fb61d
Guillermo Jimenez Iriarte <122047330+GuilleJI@users.noreply.github.com>
2023-12-21 03:37:22 -0500

    JAVASCRIPT: fetch existing items from local storage

--- main.js
+++ main.js
@@ -3,3 +3,8 @@ const ITEMS_CONTAINER = document.getElementById("items");
 const ITEM_TEMPLATE = document.getElementById("itemTemplate"); 
 const ADD_BUTTON = document.getElementById("add"); 
 // All three (items, itemTemplate, and "add" button are referenced in out JavaScript file)
+
+//Step 2: fetch the existing items from local storage.
+function getItems() {
+    //this function will look at local storage and get the items out if there are already
+}
